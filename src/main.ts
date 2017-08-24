d2a39d4c291c9cd88874d27dfcb41bb0f809e33c
naveen.sain <naveen.sain@bdbizviz.com>
2017-08-24 12:03:08 +0530

    Yujaa UI

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from './main/webapp/core/app.module';
+import { environment } from './main/webapp/environments/environment';
 
-if (environment.production) {
+/*if (environment.production) {
   enableProdMode();
-}
-
+}*/
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
